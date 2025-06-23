afdd713927d6f1cb18ed7e8b48d07cae77260580
Abhinayaa Karthigeyan <abhinayaak5@gmail.com>
2025-06-23 13:07:16 +0300

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,6 +5,7 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link";
   size?: "default" | "sm" | "lg" | "icon";
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
